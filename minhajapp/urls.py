d7d941482c1e5315f6ecc885dbd20c60495e3ef7
mi3454 <minhaj3454@gmail.com>
2026-01-14 19:28:10 +0600

    Updated home view to render home.html

--- minhajapp/urls.py
+++ minhajapp/urls.py
@@ -2,11 +2,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('home/', views.home, name='home'),
     path('profile/', views.profile, name='profile'),
     path('login/', views.login, name='login'),
     path('signup/', views.signup, name='signup'),   # 👈 এই name টা খুব গুরুত্বপূর্ণ
     path('dashboard/', views.dashboard, name='dashboard'),
     path('logout/', views.logout_view, name='logout'),
+    path('users-list/', views.users_list, name='users_list'),
 
 ]
