1c1ddbde967977459be18a54b6a2ebe303554b00
Md Minhajul Islam <113469542+mi3454@users.noreply.github.com>
2026-01-15 00:11:15 +0600

    Update urls.py

--- minhajapp/urls.py
+++ minhajapp/urls.py
@@ -8,6 +8,6 @@ urlpatterns = [
     path('signup/', views.signup, name='signup'),   # 👈 এই name টা খুব গুরুত্বপূর্ণ
     path('dashboard/', views.dashboard, name='dashboard'),
     path('logout/', views.logout_view, name='logout'),
-    path('users-list/', views.users_list, name='users_list'),
+    path('users_list/', views.users_list, name='users_list'),
 
 ]
