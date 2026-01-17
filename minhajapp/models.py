de6197809257e8f9fbedeedbf120c4c134fc8985
Md Minhajul Islam <113469542+mi3454@users.noreply.github.com>
2026-01-17 10:27:21 +0600

    Update models.py

--- minhajapp/models.py
+++ minhajapp/models.py
@@ -7,6 +7,7 @@ class Profile(models.Model):
     address = models.CharField(max_length=255, blank=True)
     bio = models.TextField(blank=True)
     profile_pic = models.ImageField(
+        default='default.jpg',
         upload_to='profile_pics/',
         blank=True,
         null=True
